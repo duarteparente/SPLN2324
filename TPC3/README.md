19599cf5a306a3cc32cc36b3253119145464ed59
duarteparente <duarte.g.parente@gmail.com>
2024-03-02 21:52:46 +0000

    TPC3

---
+++ TPC3/README.md
@@ -0,0 +1,23 @@
+## TPC3
+
+Gerador de templates de ficheiros de configuração para packages python
+
+#### Estratégia adotada
+
+São interpoladas as variáveis **author**, **email**, **project**, correspondentes às informações do autor e respetivo nome do package.
+
+###### 1º Opção
+
+Indicação do valor das variáveis configuráveis através de flags
+
+- -n : Nome do Projeto
+- -a : Nome do Autor
+- -e : Email do Autor
+
+###### 2º Opção
+
+- Caso não sejam detetadas as flags relativas às informações do autor, será consultado um eventual ficheiro de metadados ´´´.metadata.json´´´ que terá de estar situado na root, e terá de conter as chaves **author** e **email**.
+
+- Na eventualidade da falha na deteção do ficheio, a inferência do nome do autor será através do username do utilizador com sessão iniciada na linha de comandos, não sendo possível inferir o email do mesmo.
+
+- Em relação ao nome do package, este será inferido na eventual deteção de 1 único ficheiro python na atual diretoria.
---
+++ TPC3/makepyproject.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+'''
+NAME
+    makepyproject - Python packages template generator
+
+SYNOPSIS
+    makepyproject [-n <name>] [-a <author>] [-e <email>]
+    
+DESCRIPTION
+    Generates a template configuration file and creates a 'pyproject.toml' with it.
+
+    -n      Project name to be added to configuration file
+    
+    -a      Author name to be added to configuration file
+        
+    -e      Email to be added to configuration file
+'''
+import jinja2, os, json
+from jjcli import *
+from glob import glob
+
+modes = glob("*.py")
+
+vars = {
+    "project": "#FIXME",
+    "author": os.getlogin(),
+    "email": "#FIXME@",
+    "dependencies": []
+}
+
+template = jinja2.Template('''[build-system]
+requires = ["flit_core >=3.2,<4"]
+build-backend= "flit_core.buildapi"
+
+[project]
+name ="{{project}}"
+authors = [
+    {name = "{{author}}", email = "{{email}}"},
+]
+classifiers = [
+    "License :: OSI Approved :: MIT License",
+]
+requires-python = ">=3.8"
+dynamic = ["version","description"]
+
+dependencies = {{dependencies}}
+
+[project.scripts]
+{{project}} = "{{project}}:main"                     
+''')
+
+
+def project_name(cl):
+    if "-n" in cl.opt:
+        vars["project"] = cl.opt.get("-n")
+    elif len(modes) == 1:
+        vars["project"] = modes[0].replace(".py","")
+
+
+def readMetadata(cl):
+    metadata = os.path.expanduser("~/.metadata.json")
+
+    if "-a" in cl.opt:
+        vars["author"] = cl.opt.get("-a")
+    if "-e" in cl.opt:
+        vars["email"] = cl.opt.get("-e")
+    else:
+        metadata = os.path.expanduser("~/.metadata.json")
+        if os.path.isfile(metadata):
+            with open(metadata) as f:
+                data = json.load(f)
+                vars["author"] = data["author"]
+                vars["email"] = data["email"]
+
+
+def write_config_file():
+    file = open("pyproject.toml","w")
+    file.write(template.render({
+        "project": vars["project"],
+        "author": vars["author"],
+        "email": vars["email"],
+        "dependencies": vars["dependencies"]
+    }))
+
+
+def main():
+    cl = clfilter("n:a:e:", doc=__doc__)
+    project_name(cl)
+    readMetadata(cl)
+    write_config_file()   
